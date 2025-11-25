3d2323c44038a677cc24baa93108e746b84b539a
Italo Oliveira <italooliveiraxdd@gmail.com>
2025-11-24 22:57:47 -0300

    commit inicial

---
+++ src/main/java/br/com/petsaude/regula_ai_backend/RegulaAiBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.petsaude.regula_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegulaAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegulaAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petsaude/regula_ai_backend/RegulaAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petsaude.regula_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegulaAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
