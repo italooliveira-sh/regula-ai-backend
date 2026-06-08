f747e69bd96ebaabf0ef5ec4a37c1e85c4a0cca1
gbrlevi <powerzinns@gmail.com>
2026-06-07 22:14:30 -0300

    feat: query de top procedimentos

--- src/main/java/br/com/petsaude/regula_ai_backend/Repository/EncaminhamentoProcedimentoRepository.java
+++ src/main/java/br/com/petsaude/regula_ai_backend/Repository/EncaminhamentoProcedimentoRepository.java
@@ -1,11 +1,20 @@
 package br.com.petsaude.regula_ai_backend.Repository;
 
 import br.com.petsaude.regula_ai_backend.entity.EncaminhamentoProcedimento;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EncaminhamentoProcedimentoRepository extends JpaRepository<EncaminhamentoProcedimento, UUID> {
+
+    @Query("SELECT ep.procedimento.codigo, ep.procedimento.descricao, COUNT(ep) " +
+           "FROM EncaminhamentoProcedimento ep " +
+           "GROUP BY ep.procedimento.codigo, ep.procedimento.descricao " +
+           "ORDER BY COUNT(ep) DESC")
+    List<Object[]> findTopProcedimentos(Pageable pageable);
 }
