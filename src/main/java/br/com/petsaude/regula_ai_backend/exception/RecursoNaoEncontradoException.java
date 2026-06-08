f6e9a8c382b577049bba7607b8ebaad8316b95ef
gbrlevi <powerzinns@gmail.com>
2026-06-07 22:10:44 -0300

    feat: tratamento de erros da api

---
+++ src/main/java/br/com/petsaude/regula_ai_backend/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.petsaude.regula_ai_backend.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
